b27416142475cfdff8f881f9d614b81827989994
robin_shaun <robin_shaun@foxmail.com>
2021-04-18 10:09:30 +0800

    update precision_landing

--- control/precision_landing.py
+++ control/precision_landing.py
@@ -1,5 +1,6 @@
 import rospy
 from geometry_msgs.msg import Twist, PoseStamped, TransformStamped
+from std_msgs.msg import String
 from tf2_ros import TransformListener, Buffer
 import sys
 
@@ -14,23 +15,37 @@ if __name__ == '__main__':
     tfBuffer = Buffer()
     tflistener = TransformListener(tfBuffer)
     cmd_vel_enu = Twist()   
+    cmd = String()
     local_pose = PoseStamped()
     Kp = 1.0
-    land_vel = 0
+    land_vel = 0.5
+    not_find_time = 0
+    get_time = False
     rospy.Subscriber(vehicle_type+'_'+vehicle_id+"/mavros/vision_pose/pose", PoseStamped, local_pose_callback)
     cmd_vel_pub = rospy.Publisher('/xtdrone/'+vehicle_type+'_'+vehicle_id+'/cmd_vel_enu', Twist, queue_size=2)
-    rate = rospy.Rate(50)
+    cmd_pub = rospy.Publisher('/xtdrone/'+vehicle_type+'_'+vehicle_id+'/cmd', String, queue_size=2)
+    rate = rospy.Rate(20)
     while not rospy.is_shutdown():
         try:
-            tfstamped = tfBuffer.lookup_transform('tag_'+vehicle_id, 'map', rospy.Time(0))
+            tfstamped = tfBuffer.lookup_transform('map', 'tag_'+vehicle_id, rospy.Time(0))
+            get_time = False
+            print(tfstamped.transform.translation.x,tfstamped.transform.translation.y)
+            cmd_vel_enu.linear.x = Kp * (tfstamped.transform.translation.x+0.3 - local_pose.pose.position.x)
+            cmd_vel_enu.linear.y = Kp * (tfstamped.transform.translation.y - local_pose.pose.position.y)
+            cmd_vel_enu.linear.z = -land_vel
         except:
-            continue
-        # print('tf:',tfstamped.transform.translation.x)
-        # print(local_pose.pose.position.x)
-        cmd_vel_enu.linear.x = Kp * (tfstamped.transform.translation.x - local_pose.pose.position.x)
-        cmd_vel_enu.linear.y = Kp * (tfstamped.transform.translation.y - local_pose.pose.position.y)
-        cmd_vel_enu.linear.z = -land_vel
-        # print(cmd_vel_enu)
+                if not get_time:
+                    not_find_time = rospy.get_time()
+                    get_time = True
+                if rospy.get_time() - not_find_time > 2.0:
+                    cmd_vel_enu.linear.x = 0.0
+                    cmd_vel_enu.linear.y = 0.0
+                    cmd_vel_enu.linear.z = 0.0
+                    cmd = 'HOVER'
+                    print(cmd)
+                    get_time = False
         cmd_vel_pub.publish(cmd_vel_enu)
+        cmd_pub.publish(cmd)
         rate.sleep()
+
 
